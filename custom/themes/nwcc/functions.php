8fc0b6c2b0d31698107c1045d790f7136a758c67
Austin <austin.goudge@psycle.com>
2016-10-17 12:13:23 +0100

    Updated styling of non-logged in text for PM shortcode.

--- custom/themes/nwcc/functions.php
+++ custom/themes/nwcc/functions.php
@@ -12,7 +12,7 @@ function bp_private_message_shortcode($atts, $content = null) {
     if( is_user_logged_in() ) {
         return '<a href="' . wp_nonce_url( bp_loggedin_user_domain() . bp_get_messages_slug() . '/compose/?r=' . $to ) . '" title="Create private message">' . $content . '</a>';
     } else {
-        return '[Please <a href="' . wp_login_url() . '">log in</a> to access this link]';
+        return '<span style="opacity: 0.5">[Please </span><span style="opacity: 1.0"><a href="' . wp_login_url() . '">log in</a></span><span style="opacity: 0.5"> to access this link]</span>';
     }
 }
 add_shortcode( 'bp_privatemessage', 'bp_private_message_shortcode' );
